356d5c09f2b8843d282c9f4d711d7577771890d7
RitikaPathania03 <ritikapathania520@gmail.com>
2022-06-06 00:00:36 +0530

    saving the changes made in original api for bookschema assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,33 @@
-const UserModel= require("../models/userModel")
+//const UserModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
+//const createUser= async function (req, res) {
+  //  let data= req.body
+    //let savedData= await UserModel.create(data)
+    //res.send({msg: savedData})
+//}
+
+//const getUsersData= async function (req, res) {
+  //  let allUsers= await UserModel.find()
+  //  res.send({msg: allUsers})
+//}
+
+//module.exports.createUser= createUser
+//module.exports.getUsersData= getUsersData
+
+//========================================================================================================
+
+//controller for book Schema assignment
+
+const userModel = require ("../models/userModel")
+const createDetails= async function(req,res){
+    let data=req.body
+    let savedData= await userModel.create(data)
     res.send({msg: savedData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+const getDetails=async function(req,res){
+    let allBooks= await userModel.find()
+    res.send({msg: allBooks})
 }
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+module.exports.createDetails=createDetails
+module.exports.getDetails=getDetails
